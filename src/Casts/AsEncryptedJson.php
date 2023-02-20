cb97f1cba273228d9b22d04ef7a1800d1bbb0e6a
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2023-02-20 03:48:24 -0300

    Removes unnecessary Static analysis ignored line.

--- src/Casts/AsEncryptedJson.php
+++ src/Casts/AsEncryptedJson.php
@@ -57,7 +57,6 @@ class AsEncryptedJson extends AsJson
      */
     protected function encrypter(Model $model): Encrypter
     {
-        // @phpstan-ignore-next-line
         return $model::$encrypter ?? app('encrypter');
     }
 }
